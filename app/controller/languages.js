26417aed1878e55d459f039950743b3e652226f7
lzwai <kevinlzwai@gmail.com>
2018-10-18 16:46:23 +0800

    fix

--- app/controller/languages.js
+++ app/controller/languages.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class LanguagesController extends Controller {
   async getLanguages() {
     const { ctx, service } = this;
-    ctx.body = { languages: await service.i18n.findLanguages() };
+    ctx.body = { languages: await service.languages.findLanguages() };
   }
 }
 
