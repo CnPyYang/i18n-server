e3c2ab19c0a0b5a2492a7443ceb495ee1d795149
lzwai <kevinlzwai@gmail.com>
2018-11-08 11:35:02 +0800

    修改 cnd 地址为 js 地址

--- app/controller/cdn.js
+++ app/controller/cdn.js
@@ -13,22 +13,25 @@ class CdnController extends Controller {
 
     const { hostname } = ctx.request.body;
     const { site_i18n, pages_i18n } = await service.cdn.genI18n(hostname);
+    const script_wrap = json => `window.i18nData = ${json};`;
+    const suffix_replace = (page_path, suffix) => page_path.replace(/(\.[^\.]*)$/, suffix);
 
-    const site_i18n_json = JSON.stringify(site_i18n);
-    await uploadString(hostname, site_i18n_json);
+    const site_i18n_json = script_wrap(JSON.stringify(site_i18n));
+    await uploadString(`${hostname}.js`, site_i18n_json);
 
     const pages_paths = Object.keys(pages_i18n);
     const pages_upload_urls = {};
+
     for (let i = 0, len = pages_paths.length; i < len; i++) {
       const page_path = pages_paths[i];
-      const page_i18n_json = JSON.stringify(pages_i18n[page_path]);
-      await uploadString(`${hostname}${page_path}`, page_i18n_json);
-      pages_upload_urls[page_path] = `${config.qiniu.uploadUrl}/${hostname}${page_path}`;
+      const page_i18n_json = script_wrap(JSON.stringify(pages_i18n[page_path]));
+      await uploadString(`${hostname}${suffix_replace(page_path, '.js')}`, page_i18n_json);
+      pages_upload_urls[page_path] = `${config.qiniu.uploadUrl}/${hostname}${suffix_replace(page_path, '.js')}`;
     }
 
     ctx.body = {
       data: {
-        site: `${config.qiniu.uploadUrl}/${hostname}`,
+        site: `${config.qiniu.uploadUrl}/${hostname}.js`,
         pages: pages_upload_urls,
       },
     };
