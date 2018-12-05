1379fffbbd7022fb7ec38315b311c18cdfd96ef4
lzwai <kevinlzwai@gmail.com>
2018-12-05 15:02:38 +0800

    增加 json cdn 格式数据

--- app/controller/cdn.js
+++ app/controller/cdn.js
@@ -20,23 +20,38 @@ class CdnController extends Controller {
       return page_path + suffix;
     };
 
-    const site_i18n_json = script_wrap(JSON.stringify(site_i18n));
-    await uploadString(`${hostname}.js`, site_i18n_json);
+    const site_i18n_json = JSON.stringify(site_i18n);
+    await uploadString(`${hostname}.json`, site_i18n_json);
+
+    const site_i18n_js = script_wrap(site_i18n_json);
+    await uploadString(`${hostname}.js`, site_i18n_js);
 
     const pages_paths = Object.keys(pages_i18n);
-    const pages_upload_urls = {};
+    const pages_upload_js_urls = {};
+    const pages_upload_json_urls = {};
 
     for (let i = 0, len = pages_paths.length; i < len; i++) {
       const page_path = pages_paths[i];
-      const page_i18n_json = script_wrap(JSON.stringify(pages_i18n[page_path]));
-      await uploadString(`${hostname}${suffix_replace(page_path, '.js')}`, page_i18n_json);
-      pages_upload_urls[page_path] = `${config.qiniu.uploadUrl}/${hostname}${suffix_replace(page_path, '.js')}`;
+      const page_i18n_json = JSON.stringify(pages_i18n[page_path]);
+      const page_i18n_js = script_wrap(page_i18n_json);
+
+      await uploadString(`${hostname}${suffix_replace(page_path, '.js')}`, page_i18n_js);
+      pages_upload_js_urls[page_path] = `${config.qiniu.uploadUrl}/${hostname}${suffix_replace(page_path, '.js')}`;
+
+      await uploadString(`${hostname}${suffix_replace(page_path, '.json')}`, page_i18n_json);
+      pages_upload_json_urls[page_path] = `${config.qiniu.uploadUrl}/${hostname}${suffix_replace(page_path, '.json')}`;
     }
 
     ctx.body = {
       data: {
-        site: `${config.qiniu.uploadUrl}/${hostname}.js`,
-        pages: pages_upload_urls,
+        js: {
+          site: `${config.qiniu.uploadUrl}/${hostname}.js`,
+          pages: pages_upload_js_urls,
+        },
+        json: {
+          site: `${config.qiniu.uploadUrl}/${hostname}.json`,
+          pages: pages_upload_json_urls,
+        },
       },
     };
   }
